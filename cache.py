8909b3230e2e626fae98821a35bbfdd7fa490299
Ziv Attias <ziv.attias7@gmail.com>
2023-04-27 14:36:06 +0300

    add: cache class and JSON-based implementation

---
+++ cache.py
@@ -0,0 +1,29 @@
+class Cache:
+    def __init__(self) -> None:
+        self._cache: dict = {}
+
+        # Cache sample:
+        # {
+        #   id: {
+        #       status: "processing" | "complete",
+        #       result: {}
+        #   }
+        # }
+
+    def get_all(self) -> dict:
+        return self._cache
+
+    def get(self, uuid: str) -> dict:
+        return self._cache[uuid]
+
+    def add(self, uuid: str) -> dict:
+        self._cache[uuid] = {
+            "status": "processing",
+            "result": {},
+        }
+        return self._cache[uuid]
+
+    def update(self, uuid: str, status: str, result: dict):
+        self._cache[uuid]["status"] = status
+        self._cache[uuid]["result"] = result
+        return self._cache[uuid]
